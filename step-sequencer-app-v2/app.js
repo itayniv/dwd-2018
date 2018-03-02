b5cc99efa9f41917ebfc4de1fc6597ae2a38b0c4
Itay Niv <itayniv@gmail.com>
2018-03-02 15:55:29 -0500

    2nd commit

--- step-sequencer-app-v2/app.js
+++ step-sequencer-app-v2/app.js
@@ -44,15 +44,26 @@ var server = app.listen(3000, function () {
 var sockets = io(server);
 // configure socket handlers
 sockets.on('connection', function(socket){
+  //Hi this is all the users connected
+  sockets.emit('usercount', sockets.engine.clientsCount);
+  console.log('User num: ', sockets.engine.clientsCount);
+
   console.log('a user connected');
   socket.on('sendStep', function(data){
     seqarraystate = data.theData;
     sockets.emit('sendSteps', seqarraystate);
-    console.log('emmiting array', seqarraystate);
+    // console.log('emmiting array', seqarraystate);
   });
-  // socket.on('my other event', function (tempo) {
-  //   console.log(tempo);
-  // });
+
+  socket.on('disconnect', function(){
+    //Hi somebody dissconencted we have a different count!
+    sockets.emit('usercount', sockets.engine.clientsCount);
+    console.log('User num: ', sockets.engine.clientsCount);
+  });
+//   setTimeout(function() {
+//   io.sockets.emit('message');
+// }, 1000 );
+
 });
 
 
@@ -62,14 +73,21 @@ var metronome = new Metronome();
 // But you could also initialize one at another tempo.
 // It emits a 'tick' event on each beat
 metronome.set(200);
-metronome.on('tick', function logBPM(){
+
+metronome.on('tick', function(){
   //console.log(metronome.bpm + ' bpm');
   //currplayer = Math.floor((new Date()).getTime()%(width*appTempo)/appTempo);
   currplayer ++;
   if (currplayer == 16){
     currplayer = 0;
   }
-  console.log(currplayer);
+    sockets.emit('currplayer', currplayer);
+    //CLient should look like this!
+    // client.on('currplayer', function(n,z,b){
+    //
+    // })
+    // console.log('yay');
+  // console.log('currPlayer ', currplayer);
 
   //TODO///this is where I want to Emit to sockets
 
